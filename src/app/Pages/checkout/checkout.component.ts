aa9c331bc94d065964ae40b2c07249d03e0f2b9d
mohamedNaiem2000 <mohamednaiem735@gmail.com>
2023-03-03 04:13:30 +0200

    checkout

--- src/app/Pages/checkout/checkout.component.ts
+++ src/app/Pages/checkout/checkout.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ServiceService } from 'src/app/Service/service.service';
 @Component({
   selector: 'app-checkout',
   templateUrl: './checkout.component.html',
   styleUrls: ['./checkout.component.css']
 })
-export class CheckoutComponent {
+export class CheckoutComponent implements OnInit {
+  constructor(public myService: ServiceService) { }
 
+  token = localStorage.getItem('token')
+address:any;
+  ngOnInit(): void {
+    this.myService.getProductCart(this.token).subscribe({
+      next: (res) => {
+        this.address = res;
+        console.log(res);
+        // this.rate= this.products.rate;
+      },
+      error: (err) => {
+        console.log(err);
+      },
+    });
+  }
 }
